73053e7708f6bbcd23354cd50d2a266a2f36264e
Paul BEDROSSIAN <paul.bedrossian92@gmail.com>
2023-04-03 15:38:01 +0200

    VERSION 1.2

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-# Run the GUI
-import tkinter as tk
-import os
-import application.GUI.window as Application
+def main():
+    import tkinter as tk
+    import os
+    import application.GUI.window as Application
 
-root = tk.Tk()
-gui = Application.Window(root)
-root.mainloop()
-PATH = ["src/tmp/noise_reshape.png", "src/tmp/noise.png", "src/tmp/noise_psd.png", "src/tmp/noise_psd_reshape.png", "src/tmp/noise_hist.png", "src/tmp/noise_hist_reshape.png"]
-for p in PATH:
-    try: 
-        os.remove(p)
-    except:
-        pass
+    root = tk.Tk()
+    gui = Application.Window(root)
+    root.mainloop()
+    PATH = ["src/tmp/noise_reshape.png", "src/tmp/noise.png", "src/tmp/noise_psd.png", "src/tmp/noise_psd_reshape.png", "src/tmp/noise_hist.png", "src/tmp/noise_hist_reshape.png"]
+    for p in PATH:
+        try: 
+            os.remove(p)
+        except:
+            pass
+
+if __name__ == "__main__":
+    main()
