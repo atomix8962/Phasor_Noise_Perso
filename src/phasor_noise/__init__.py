8b0320bee1d85a858602434eb730739d98f6d026
etachouzin <etienne.tachouzin64@gmail.com>
2023-05-18 17:54:05 +0200

    docstring and fixing unused variable

--- src/phasor_noise/__init__.py
+++ src/phasor_noise/__init__.py
@@ -1,10 +1,13 @@
 def default_window():
+    """
+    Easy first window to manipulate the phasor noise and observe it
+    """
     import tkinter as tk
     import os
-    import src.phasor_noise.GUI as app
+    from src.phasor_noise.GUI import Window
 
     root = tk.Tk()
-    gui = app.Window(root)
+    Window(root)
     root.mainloop()
     path = ["src/tmp/noise_reshape.png", "src/tmp/noise.png", "src/tmp/noise_psd.png", "src/tmp/noise_psd_reshape.png",
             "src/tmp/noise_hist.png", "src/tmp/noise_hist_reshape.png"]
