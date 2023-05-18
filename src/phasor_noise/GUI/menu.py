ae125e6a69acc7d3debc50a0a2fd483181744eb0
etachouzin <etienne.tachouzin64@gmail.com>
2023-05-18 17:49:05 +0200

    docstring update and linting PEP8
    docstring update and linting PEP8
    
    docstring update and linting PEP8

--- src/phasor_noise/GUI/menu.py
+++ src/phasor_noise/GUI/menu.py
@@ -1,13 +1,18 @@
 import tkinter as tk
 
-class Menu():
-    def __init__(self, gui) -> None:
-        #Main top menu
-        mainmenu  = tk.Menu (relief=tk.RAISED, borderwidth=3)
 
-        #Version chooser section
-        version= tk.Menu(mainmenu, tearoff=0)
-        mainmenu.add_cascade(label= "Version", menu = version)
+class Menu:
+    """
+    Menu is class to create the different sections of the window
+    """
+    def __init__(self, gui) -> None:
+        """
+        Set the main menu on top and set the version with parameters and save and load it
+        """
+        mainmenu = tk.Menu(relief=tk.RAISED, borderwidth=3)
+
+        version = tk.Menu(mainmenu, tearoff=0)
+        mainmenu.add_cascade(label="Version", menu=version)
         version.add_radiobutton(label="Python")
         version.add_radiobutton(label="Numpy")
         version.add_radiobutton(label="JAX")
@@ -15,21 +20,17 @@ class Menu():
         version.entryconfigure("Numpy", command=gui.change_mode_numpy)
         version.entryconfigure("JAX", command=gui.change_mode_jax)
 
-        #Parameters chooser section
         param = tk.Menu(mainmenu, tearoff=0)
-        mainmenu.add_cascade(label= "Paramètres", menu=param)
+        mainmenu.add_cascade(label="Paramètres", menu=param)
 
-        #Save configuration 
         save = tk.Menu(mainmenu, tearoff=0)
         param.add_cascade(label="Sauvegarde des données", menu=save)
         save.add_command(label="Sauvegarder la configuration", command=gui.save)
 
-        #Visualize options
-        visu= tk.Menu(mainmenu, tearoff=0)
+        visu = tk.Menu(mainmenu, tearoff=0)
         param.add_cascade(label="Visualisation", menu=visu)
         visu.add_separator()
         visu.add_checkbutton(label="Afficher les noyaux", onvalue=1, offvalue=0, variable=gui.visualize)
         visu.add_separator()
 
-        #Load the menu
-        gui.master.config(menu = mainmenu)
+        gui.master.config(menu=mainmenu)
