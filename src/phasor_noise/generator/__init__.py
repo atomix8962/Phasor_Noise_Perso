efd7880c003d4dc38fef8f4cbec8d09acc8d03a3
etachouzin <etienne.tachouzin64@gmail.com>
2023-05-18 17:38:20 +0200

    variable should be lowercase

--- src/phasor_noise/generator/__init__.py
+++ src/phasor_noise/generator/__init__.py
@@ -1,22 +1,8 @@
-"""
-Nom :           phasor/phasor_py.py
-Decription :    Script d'application de bruit Phasor en utilisant uniquement python
-Date :          30/01/2023
-Auteur :        Alexis
-Statut :        Fonctionne en v1.0 (A faire relire par Alexis)
-"""
+"""Script d'application de bruit Phasor en utilisant uniquement python"""
 import math
 import numpy as np
 
 
-# Section des imports   
-
-# Section de définition des fonctions
-"""
-apply_function applique à une matrice la fonction de bruit Phasor avec les noyaux passés en arguments
-Matrice : X, Y -> np.meshgrid()
-Un noyau est défini par : [position, direction, frequence, largeur]
-"""
 class PhasorGenerator:
     """
     Classe inutilisable dans l'état doit redéfinir:
@@ -71,11 +57,11 @@ class PhasorGenerator:
         """
         raise NotImplemented()
 
-    def gaussian(self, vector:list, bandwidth:float) -> float:
+    def gaussian(self, vector: list, bandwidth: float) -> float:
         """
         gaussian retourne le résultat du vecteur (vector) par la fonction gaussienne avec une largeur définie (bandwidth)
         """
-        return self.exp(-math.pi*(bandwidth**2)*self.dot(vector, vector))
+        return self.exp(-math.pi * (bandwidth ** 2) * self.dot(vector, vector))
 
     def polar_coordinates_coefficients(self, x: list, kernel_array: list) -> list:
         """
@@ -83,8 +69,8 @@ class PhasorGenerator:
         """
         return [self.gaussian(self.vector_subtraction(x, kernel[0]), kernel[3]) * self.sin(
             -self.dot(self.vector_subtraction(x, kernel[0]), kernel[1]) * kernel[2]) for kernel in kernel_array], \
-               [self.gaussian(self.vector_subtraction(x, kernel[0]), kernel[3]) * self.cos(
-                   -self.dot(self.vector_subtraction(x, kernel[0]), kernel[1]) * kernel[2]) for kernel in kernel_array]
+            [self.gaussian(self.vector_subtraction(x, kernel[0]), kernel[3]) * self.cos(
+                -self.dot(self.vector_subtraction(x, kernel[0]), kernel[1]) * kernel[2]) for kernel in kernel_array]
 
     def phase_func(self, x: list, kernel_array: list) -> float:
         """
@@ -99,14 +85,17 @@ class PhasorGenerator:
         """
         return self.sin(self.phase_func(x, kernel_array))
 
+
 def timeperf(func):
     def inner(*args, **kargs):
         import time
         begin = time.perf_counter_ns()
         result = func(*args, **kargs)
         end = time.perf_counter_ns()
-        return result, end-begin
+        return result, end - begin
+
     return inner
+
 
 class PythonPhasorGenerator(PhasorGenerator):
 
@@ -123,10 +112,11 @@ class PythonPhasorGenerator(PhasorGenerator):
         return math.atan2(x, y)
 
     def dot(self, vector1, vector2):
-        return vector1[0]*vector2[0]+vector1[1]*vector2[1]
+        return vector1[0] * vector2[0] + vector1[1] * vector2[1]
 
     def vector_subtraction(self, vector1: list, vector2: list):
-        return [vector1[0]-vector2[0], vector1[1]-vector2[1]]
+        return [vector1[0] - vector2[0], vector1[1] - vector2[1]]
+
 
 class NumpyPhasorGenerator(PhasorGenerator):
 
@@ -143,33 +133,41 @@ class NumpyPhasorGenerator(PhasorGenerator):
         return np.arctan2(x, y)
 
     def dot(self, vector1, vector2):
-        return vector1[0]*vector2[0]+vector1[1]*vector2[1]
+        return vector1[0] * vector2[0] + vector1[1] * vector2[1]
 
     def vector_subtraction(self, vector1: list, vector2: list):
-        return vector1-vector2
+        return vector1 - vector2
+
 
 @timeperf
-def apply_noise_python(X:list, Y:list, kernels:list) -> list:
+def apply_noise_python(x: list, y: list, kernels: list) -> list:
+    """
+    apply_function applique à une matrice la fonction de bruit Phasor avec les noyaux passés en arguments
+    Matrice : X, Y -> np.meshgrid()
+    Un noyau est défini par : [position, direction, frequence, largeur]
+    """
     Z = list()
     phasor_generator = PythonPhasorGenerator()
-    for i in range(len(X)):
+    for i in range(len(x)):
         Z.insert(i, [])
-        for j in range(len(Y)):
-            vector = X[i][j], Y[i][j]
-            Z[i].insert(j, phasor_generator.phasor_noise(vector, kernels)) # On redéfinit le pixel par la nouvelle valeur calculer avec le bruit Phasor
+        for j in range(len(y)):
+            vector = x[i][j], y[i][j]
+            Z[i].insert(j, phasor_generator.phasor_noise(vector,
+                                                         kernels))
     return Z
 
-def create_numpy_kernel(kernels, sizeX, sizeY):
+
+def create_numpy_kernel(kernels, size_x, size_y):
     for i in range(len(kernels)):
-        kernels[i][0] = np.array([np.full((sizeX, sizeY), kernels[i][0][0]),
-                               np.full((sizeX, sizeY), kernels[i][0][1])])
-        kernels[i][1] = np.array([np.full((sizeX, sizeY), kernels[i][1][0]),
-                               np.full((sizeX, sizeY), kernels[i][1][1])])
+        kernels[i][0] = np.array([np.full((size_x, size_y), kernels[i][0][0]),
+                                  np.full((size_x, size_y), kernels[i][0][1])])
+        kernels[i][1] = np.array([np.full((size_x, size_y), kernels[i][1][0]),
+                                  np.full((size_x, size_y), kernels[i][1][1])])
     return kernels
 
-@timeperf
-def apply_noise_numpy(X:list, Y:list, kernels:list, size:list) -> list:
-    kernels = create_numpy_kernel(kernels, size[0], size[1])
-    Z = NumpyPhasorGenerator().phasor_noise(np.array([X, Y]), kernels)
-    return Z
 
+@timeperf
+def apply_noise_numpy(x: list, y: list, kernels: list, size: list) -> list:
+    kernels = create_numpy_kernel(kernels, size[0], size[1])
+    Z = NumpyPhasorGenerator().phasor_noise(np.array([x, y]), kernels)
+    return Z
