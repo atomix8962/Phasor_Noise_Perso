1e41f50659578dd8bfc3d7ba11d52e1a5d9a62d2
etachouzin <etienne.tachouzin64@gmail.com>
2023-05-20 13:14:49 +0200

    Move setuping directory from default_window() to setup.py to auto-generate folder while pip install

--- src/phasor_noise/__init__.py
+++ src/phasor_noise/__init__.py
@@ -1,6 +1,6 @@
 import tkinter as tk
-import os
 from phasor_noise.GUI import Window
+
 
 def default_window():
     """
@@ -10,10 +10,3 @@ def default_window():
     root = tk.Tk()
     Window(root)
     root.mainloop()
-    path = ["src/tmp/noise_reshape.png", "src/tmp/noise.png", "src/tmp/noise_psd.png", "src/tmp/noise_psd_reshape.png",
-            "src/tmp/noise_hist.png", "src/tmp/noise_hist_reshape.png"]
-    for p in path:
-        try:
-            os.remove(p)
-        except:
-            pass
