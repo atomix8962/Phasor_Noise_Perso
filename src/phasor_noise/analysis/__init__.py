f1a4b76e5130c87d84eb7cb1c68a5db781bb6157
etachouzin <etienne.tachouzin64@gmail.com>
2023-05-20 11:46:47 +0200

    Update DocString and remove minus of PSD

--- src/phasor_noise/analysis/__init__.py
+++ src/phasor_noise/analysis/__init__.py
@@ -1,27 +1,52 @@
 import numpy as np
 
 
-def PSD(signal: list) -> np.array:
+def PSD(signal: list) -> np.ndarray:
     """
-    PSD = |x̂(f)|² with x̂ the Fourier Transform of the Signal scale by the time of integration
-    :param signal : a ndarray or list containing the evaluated value by a function
-    return : the Power Spectral Density of a signal
+    PSD = |x̂(f)|² with x̂ the Fourier Transform of the Signal scale by the time of integration.
+
+    Parameters
+    ----------
+    signal : ndarray or list
+        Containers of evaluated value by a function.
+
+    Returns
+    -------
+    y : ndarray
+        The Power Spectral Density of a signal.
     """
     f = np.fft.fft2(signal)
     f_s = np.fft.fftshift(f)
-    resp = np.power(np.absolute(f_s), 2)
-    return -resp
+    return np.power(np.absolute(f_s), 2)
 
 
 def mean(noise_array: list) -> float:
     """
-    mean permet de calculer la moyenne des pixels passés en arguments
+    Mean retrieve the average from an array.
+
+    Parameters
+    ----------
+    noise_array : ndarray or list
+        Data containers.
+    Returns
+    -------
+    y : float
+        Mean of the parameters.
     """
     return np.average(noise_array)
 
 
 def std_gap(noise_array: list) -> float:
     """
-    std_gap permet de calculer l'écart-type des pixels passés en arguments
+    std_gap retrieve the standard deviation of parameters.
+
+    Parameters
+    ----------
+    noise_array : ndarray or list
+        Data containers.
+    Return
+    ------
+    y : float
+        Standard deviation of parameters.
     """
     return np.std(noise_array)
